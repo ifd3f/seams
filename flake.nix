060384cc539df808807b446d24d8af9b014c82c9
Astrid Yu <astrid@astrid.tech>
2024-02-18 14:44:13 -0800

    Refactor nix and fix example builds

--- flake.nix
+++ flake.nix
@@ -6,37 +6,50 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, naersk, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let runtimePrograms = pkgs: with pkgs; [ graphviz nodePackages.katex ];
+    in {
+      lib.makeSite = { pkgs, name, content
+        , seams ? self.packages.${pkgs.system}.seams
+        , styles ? self.packages.${pkgs.system}.styles }:
+        pkgs.runCommand name {
+          buildInputs = [ seams ] ++ runtimePrograms pkgs;
+        } ''
+          shopt -s dotglob
+
+          mkdir -p $out
+          seams build ${content} -o $out --script-assets ${./js}
+          cp -r ${styles}/* $out/
+        '';
+
+    } // flake-utils.lib.eachDefaultSystem (system:
       let
         lib = pkgs.lib;
         pkgs = import nixpkgs { inherit system; };
+
         naersk-lib = pkgs.callPackage naersk { };
-        buildPrograms = with pkgs; [ graphviz nodePackages.katex ];
-
-        seams = naersk-lib.buildPackage (builtins.filterSource
-          (path: _: # path is of the format /nix/store/hash-whatever/Cargo.toml
-            let rootDirName = builtins.elemAt (lib.splitString "/" path) 4;
-            in builtins.elem rootDirName [
-              ".cargo"
-              "native"
-              "src"
-
-              "build.rs"
-              "Cargo.lock"
-              "Cargo.toml"
-            ]) ./.);
-
         styles = pkgs.callPackage ./styles { };
 
-        makeSite = name: content:
-          with pkgs;
-          runCommand name { buildInputs = [ seams ]; } ''
-            shopt -s dotglob
+        rustDeps = with pkgs;
+          [ openssl pkg-config iconv ]
+          ++ lib.optional (system == "aarch64-darwin")
+          [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-            mkdir -p $out
-            seams build ${content} -o $out
-            cp -r ${styles}/* $out/
-          '';
+        seams = naersk-lib.buildPackage {
+          src = builtins.filterSource
+            (path: _: # path is of the format /nix/store/hash-whatever/Cargo.toml
+              let rootDirName = builtins.elemAt (lib.splitString "/" path) 4;
+              in builtins.elem rootDirName [
+                ".cargo"
+                "native"
+                "src"
+                "test_data"
+
+                "build.rs"
+                "Cargo.lock"
+                "Cargo.toml"
+              ]) ./.;
+          buildInputs = rustDeps;
+        };
 
         python = pkgs.python3.withPackages (ps: with ps; [ pyyaml click ]);
 
@@ -44,10 +57,18 @@
         packages = rec {
           inherit styles seams;
           default = seams;
-          test-site = makeSite "test-site" ./test_data/contentdir_example;
-          astrid-dot-tech-test-site = makeSite "astrid-dot-tech-test-site"
-            ./test_data/astrid_dot_tech_example;
+          test-site = self.lib.makeSite {
+            inherit pkgs;
+            name = "test-site";
+            content = ./test_data/contentdir_example;
+          };
+          astrid-dot-tech-test-site = self.lib.makeSite {
+            inherit pkgs;
+            name = "astrid-dot-tech-test-site";
+            content = ./test_data/astrid_dot_tech_example;
+          };
         };
+
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
@@ -58,16 +79,11 @@
               rustPackages.clippy
               sass
               just
-              openssl
-              pkg-config
               python
-              iconv
               nodePackages.npm
               nodejs_21
-            ] ++ buildPrograms
-              ++ lib.optional (system != "aarch64-darwin") [ backblaze-b2 ]
-              ++ lib.optional (system == "aarch64-darwin")
-              [ darwin.apple_sdk.frameworks.SystemConfiguration ];
+            ] ++ rustDeps ++ runtimePrograms pkgs
+              ++ lib.optional (system != "aarch64-darwin") [ backblaze-b2 ];
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
       });
